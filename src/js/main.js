f0468f56ec065d43bf6bb0475eaf9e474b43fe3f
RotGoat <fncwin@mail.ru>
2022-11-03 22:54:25 +0300

    немного джса

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,109 @@
 const link = 'https://kinobd.ru/api/films';
+const root = document.getElementById("preview-cards");
+let store = {
+    nameRussian: '',
+    smallPoster: '',
+    ratingKp: 0,
+    yearStart: 0,
+    countryRu: '',
+    genres: [0]
+}
 
 const fetchData = async() => {
     const result = await fetch(link);
-    const data = await result.json();
+    const dataset = await result.json();
 
-    console.log(data);
-}
+    const datamata = dataset.data;
+    const {
+        name_russian: nameRussian,
+        name_original: nameOriginal,
+        small_poster: smallPoster,
+        year_start: yearStart,
+        time_minutes: timeMinutes,
+        genres: genres,
+        name_ru: nameRu,
+    } = datamata;
+
+    store = {
+        ...store,
+        nameRussian,
+        nameOriginal,
+        smallPoster,
+        yearStart,
+        timeMinutes,
+        genres,
+        nameRu
+    };
+
+    //const renderComponent = () => {
+    //    markup(datamata);
+    //}
+    //renderComponent()
+    function asd() {
+        let films = datamata.map(({ name_russian, name_original, small_poster }) => name_russian + ' ' + name_original)
+
+        films.forEach(film => {
+            const movieEl = document.createElement('div');
+            movieEl.classList.add("grid__item");
+            movieEl.classList.add("-ibg");
+            movieEl.innerHTML = `
+                    ${film}
+                `
+            console.log(film);
+
+            root.appendChild(movieEl)
+        });
+
+    }
+    asd()
+};
+
+//const markup = (data) => {
+
+//    data.forEach((element) => {
+//        const { nameRussian, nameOriginal, smallPoster, yearStart, timeMinutes, genres } = store;
+//        const movieEl = document.createElement('div');
+//        movieEl.classList.add("grid__item");
+//        movieEl.classList.add("-ibg");
+//        element = movieEl;
+//        element.innerHTML =
+//            `
+//               ${data.nameRussian}
+
+//          `;
+
+//        //element.innerHTML =
+//        //    `
+//        //        ${nameRussian}
+//        //         ${nameOriginal}
+//        //        <img src='${smallPoster}'/>
+//        //        ${yearStart}
+//        //        ${timeMinutes}
+//        //   `;
+//        console.log(data[0].nameRussian);
+
+//        root.appendChild(element)
+//    });
+
+//}
+
+//function showMovies(data) {
+//    const moviesEL = document.querySelector('.main__grid');
+
+//    data.forEach((movie) => {
+//        const movieEl = document.createElement('div');
+//        movieEl.classList.add("grid__item");
+//        movieEl.innerHTML = `
+//            ${nameRussian} , ${nameOriginal}
+//            <img src='${smallPoster}'/>
+//            ${yearStart}
+//            ${timeMinutes}
+//            ${genres.map(({ name_ru: nameRu }) => (' ' + nameRu))}
+//        `;
+//        moviesEL.appendChild(movieEl)
+//    });
+//}
+
+
 
 fetchData();
